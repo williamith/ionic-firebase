d754204e21a8e3f69d6f0cabd14f98d96b429d45
CIST2932Spring2019 <william.ith13@gmail.com>
2019-04-17 07:03:11 -0400

    completed announcements services and UI

--- src/app/announcements/announcements.page.ts
+++ src/app/announcements/announcements.page.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { AnnouncementsService } from './shared/announcements.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-announcements',
   templateUrl: 'announcements.page.html',
   styleUrls: ['announcements.page.scss']
 })
-export class AnnouncementsPage {}
+export class AnnouncementsPage implements OnInit{
+  announcements = [];
+
+  constructor(private announcementsService: AnnouncementsService) {}
+
+  ngOnInit() {
+    this.announcementsService.getAnnouncements()
+      .subscribe(
+        response => this.announcements = Object.values(response),
+        error => console.log(error),
+        () => { this.announcements = this.announcements.sort(((a, b) => (a.date < b.date) ? 1 : ((b.date < a.date) ? -1 : 0))); }
+      );
+  }
+}
